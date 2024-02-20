66960772a7c439a44e3b896ffb0acdaed9cc9dbf
Anderson Silva <andersonsrocha.dev@gmail.com>
2024-02-19 21:22:29 -0300

    chore(preview) :wrench: adicionar preview no projeto.

--- vite.preview.ts
+++ vite.preview.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/notyist/",
   resolve: {
     mainFields: ["module"],
     alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
